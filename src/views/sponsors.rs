88c358eeca5fa8505876cdf9de1e29a0718566a6
Tyler-Stocks <tylerjstocks@gmail.com>
2025-05-14 14:46:42 -0700

    Continued working on sponsors page.

--- src/views/sponsors.rs
+++ src/views/sponsors.rs
@@ -1,78 +1,66 @@
 use dioxus::prelude::*;
+use dioxus_resize_observer::use_size;
+use dioxus_use_mounted::use_mounted;
 use crate::views::Footer;
 
-const CSS: Asset = asset!("assets/styles/sponsors.css");
+const CSS: Asset = asset!("/assets/styles/sponsors.css");
 
-const BRAMELY_HOUSE_ENTERPRISES_LOGO: Asset = asset!("assets/images/Bramley House Enterprises Logo.avif");
-const COUNTRY_GROCER_LOGO: Asset = asset!("assets/images/Country Grocer Logo.avif");
-const HOWELL_DATA_SYSTEMS_LOGO: Asset = asset!("assets/images/Howell Data Systems Logo.avif");
-const TEAM_PHOTO: Asset = asset!("assets/images/sponsor_us.avif");
+// Engineer Tier Sponsors
+const BRAMELY_HOUSE_ENTERPRISES_LOGO: Asset = asset!("/assets/images/Bramley House Enterprises Logo.avif");
+const COUNTRY_GROCER_LOGO: Asset = asset!("/assets/images/Country Grocer Logo.avif");
+const HOWELL_DATA_SYSTEMS_LOGO: Asset = asset!("/assets/images/Howell Data Systems Logo.avif");
 
-#[derive(Copy, Clone, PartialEq, PartialOrd)]
-enum SponsorSide {
-  LEFT,
-  RIGHT
-}
+// Mechanic Tier Sponsors
+const BERNHARDT_CONTRACTING_LOGO: Asset = asset!("/assets/images/Bernhardt Contracting Logo.png");
+
+// Other photos
+const TEAM_PHOTO: Asset = asset!("/assets/images/sponsor_us.avif");
 
 #[component]
-fn EngineerSponsor(
+fn EngineerSponsorLeft(
   name: &'static str,
   description: &'static str,
   image: Asset,
-  link: &'static str,
-  side: SponsorSide
+  link: Option<&'static str>,
 ) -> Element {
-  let className = match side {
-    SponsorSide::LEFT  => "EngineerSponsor EngineerSponsorRight",
-    SponsorSide::RIGHT => "EngineerSponsor EngineerSponsorLeft"
-  };
-
   rsx! {
     div {
-      class: className,
+      class: "EngineerSponsor EngineerSponsorLeft",
 
-      match side {
-        SponsorSide::LEFT => rsx! {
-          if !link.is_empty() {
-            a { href: link, target: "_blank", rel: "noreffere", img { src: image } }
-          } else {
-            img { src: image }
-          }
-          div {
-            class: "EngineerSponsorDescription",
-            h2 { "{name}" },
-            p { "{description}" }
-          }
-        },
-        SponsorSide::RIGHT => rsx! {
-          div {
-            class: "EngineerSponsorDescription",
-            h2 { "{name}" }
-            p { "{description}" }
-
-          }
-          if !link.is_empty() {
-            a { href: link, target: "_blank", rel: "norferrer", img { src: image } }
-          } else {
-            img { src: image }
-          }
-        }
+      img {
+        src: image,
+        if link.is_some() { a { href: link.unwrap(), target: "_blank", rel: "noreferrer" } }
       }
-
-
+      
+      div {
+        class: "EngineerSponsorDescription",
+        h3 { "{name}" },
+        p { "{description}" }
+      }
     }
   }
 }
 
 #[component]
-fn MechanicSponsor(image: Asset, link: &'static str) -> Element {
+fn EngineerSponsorRight(
+  name: &'static str, 
+  description: &'static str, 
+  link: Option<&'static str>, 
+  image: Asset
+) -> Element {
   rsx! {
     div {
-      class: "MechanicSponsor",
+      class: "EngineerSponsor EngineerSponsorRight",
+     
       div {
-        class: "MechanicSponsorImageContainer",
-        img { src: image }
-        a { href: link, target: "_blank", rel: "noreferrer" }
+        class: "EngineerSponsorDescription",
+        h3 { "{name}" }
+        p { "{description}" }
+      }
+      
+      img { 
+        src: image,
+        if link.is_some() { a { href: link.unwrap(), target: "_blank", rel: "noreferrer" } }
       }
     }
   }
@@ -81,11 +69,14 @@ fn MechanicSponsor(image: Asset, link: &'static str) -> Element {
 #[component]
 pub fn Sponsors() -> Element {
   let show_menu = use_context::<Signal<bool>>();
+  let mounted = use_mounted();
+  let size = use_size(mounted);
 
   rsx! {
     document::Stylesheet { href: CSS }
 
     main {
+      onmounted: move |event| mounted.onmounted(event),
       class: "Sponsors",
       style: if *show_menu.read() { "overflow: hidden" } else { "overflow: initial"},
       section {
@@ -93,59 +84,80 @@ pub fn Sponsors() -> Element {
         div {
           class: "ThankYouContainer",
           h1 { "Thank You" }
-          p {
-            r#"Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
-            Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu.
-            In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus.
-            Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum.
-            In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus.
-            Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum.
-            "#
+          
+          match size.width() as u64 {
+            0..1200 => {
+              rsx! { 
+                p {
+                  r#"Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus."#
+                } 
+              }
+            } 
+            1200.. => {
+              rsx! {
+                p {
+                  r#"Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+                  Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu.
+                  In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus.
+                  Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum.
+                  In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus.
+                  Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum.
+                  "#
+                } 
+              }   
+            }
           }
+          
         }
-        figure {
-          img { src: TEAM_PHOTO, alt: "Team Image" }
-          figcaption { "Team Photo At Houston World Championship 2025" }
+        div {
+          class: "SponsorsHeaderImageContainer",
+          figure {
+            img { src: TEAM_PHOTO, alt: "Team Image" }
+            figcaption { "Team Photo At Houston World Championship 2025" }
+          }
         }
       }
       section {
-        class: "EngineerSection",
+        class: "SponsorTier",
         div {
-          class: "EngineerHeader",
-          h1 { "Engineer" }
-          p { "Anyone who has donated more than $1000!" }
+          class: "SponsorTierHeader",
+          h2 { "Engineer" }
+          p { "Anyone who has donated more than $1000!" },
         }
 
         div {
-          class: "EngineerSponsors",
-          EngineerSponsor {
-            name: "Country Grocer",
+          class: "SponsorTierElementsContainer",
+          EngineerSponsorRight {
+             name: "Country Grocer",
             description: "They are a cool store. I remember this one time I bought a thing from them and I was like this thing is so country. But that kinda makes sense cause its the country grocer which is pretty cool",
             image: COUNTRY_GROCER_LOGO,
-            link: "https://www.countrygrocer.com/",
-            side: SponsorSide::RIGHT
+            link: Some("https://www.countrygrocer.com/"),
           }
-          EngineerSponsor {
-            name: "Bramley House Enterprises",
+          EngineerSponsorLeft {
+             name: "Bramley House Enterprises",
             description: "They do be doing some epic houses n' stuff. They also gave us big money so that's pretty sick of them.",
             image: BRAMELY_HOUSE_ENTERPRISES_LOGO,
-            link: "",
-            side: SponsorSide::LEFT
+            link: None,
           }
-          EngineerSponsor {
+          EngineerSponsorRight {
             name: "Howell Data Systems",
-            description: "They do data and other imporatant datistical things invloving data. They also do POS I think so that's pretty imporant",
+            description: "They do data and other important datistical things involving data. They also do POS I think so that's pretty important",
             image: HOWELL_DATA_SYSTEMS_LOGO,
-            link: "",
-            side: SponsorSide::RIGHT
+            link: Some("https://www.howelldatasystems.com/"),
           }
         }
       }
-      // section {
-      //   class: "Mechanic",
-      //   h1 { "Mechanic" }
-      //   p { "Anyone who has donated!" }
-      // }
+      section {
+        class: "SponsorTier",
+        div {
+          class: "SponsorTierHeader",
+          h2 { "Mechanic" }
+          p { "Anyone who has donated more than $500!" }
+        }
+        div {
+          class: "SponsorTierElementContainer"
+        } 
+      }
     }
     Footer {}
   }
