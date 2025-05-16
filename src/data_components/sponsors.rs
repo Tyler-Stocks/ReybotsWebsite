b5addfb98d6fa672ac29e785e0f94e14494cd529
Tyler Stocks <tylerjstocks@gmail.com>
2025-05-16 13:08:50 -0700

    Merged EngineerSponsor, MechanicSponsor, etc... into Sponsor struct. Now determining what tier a sponsor should be placed in will be handled by views::sponsors.rs . Added sponsors.json to store information about each sponsor.

--- src/data_components/sponsors.rs
+++ src/data_components/sponsors.rs
@@ -3,24 +3,11 @@ use serde::{Deserialize, Serialize};
 // Logo paths are relative to the crate root: /assets/images/{Logo Name}
 
 #[derive(Debug, Eq, PartialEq, Ord, PartialOrd, Serialize, Deserialize)]
-pub enum Sponsor {
-    CreatorSponsor {
-        name: String,
-        link: String,
-        logo_path: String,
-        low_resolution_logo_path: String,
-    },
-    MechanicSponsor {
-        name: String,
-        link: String,
-        logo_path: String,
-        low_resolution_logo_path: String
-    },
-    EngineerSponsor {
-        name: String,
-        description: String,
-        link: String,
-        logo_path: String,
-        low_resolution_logo_path: String,
-    }
+#[serde(rename_all = "camelCase")]
+pub struct Sponsor {
+    name: String,
+    description: String,
+    link: String,
+    logo_file_name: String,
+    amount_donated: u32
 }
