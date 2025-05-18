afe1ebb6d077d0986964df56f7fe21652a92a949
Tyler Stocks <tylerjstocks@gmail.com>
2025-05-17 20:23:58 -0700

    Removed left over comments, originally from the dioxus starter template, from pages/mod.rs .

--- src/pages/mod.rs
+++ src/pages/mod.rs
@@ -1,13 +1,3 @@
-//! The pages module contains the data_types for all Layouts and Routes for our app. Each layout and route in our [`Route`]
-//! enum will render one of these data_types.
-//!
-//!
-//! The [`HomePage`] and [`Blog`] data_types will be rendered when the current route is [`Route::Home`] or [`Route::Blog`] respectively.
-//!
-//!
-//! The [`Navbar`] component will be rendered on all pages of our app since every page is under the layout. The layout defines
-//! a common wrapper around all child routes.
-
 mod home;
 pub use home::HomePage;
 
